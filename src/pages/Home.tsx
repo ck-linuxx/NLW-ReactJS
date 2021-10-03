9f87207194acddb0f3caa7b443b104eb4043f198
ck-linuxx <you@example.com>
2021-10-03 14:11:36 -0300

    Estilo da Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import  illustrationImg from "../assets/images/illustration.svg"
 import logoImg from "../assets/images/logo.svg"
 import googleIconImg from "../assets/images/google-icon.svg"
 
+import { Button } from "../components/Button"
+
 import "../styles/auth.scss"
 
 export function Home() {
@@ -13,23 +15,21 @@ export function Home() {
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
+                <div className="separator">Ou entre em uma sala</div>
+                    <form>
+                        <input 
+                        type="text"
+                        placeholder="Digite o código da sala" 
+                        />
+                        <Button type="submit">Entrar na sala</Button>
+                    </form>
                 </div>
-                <div>
-                    Ou entre em uma sala
-                </div>
-                <form>
-                    <input 
-                    type="text"
-                    placeholder="Digite o código da sala" 
-                    />
-                    <button type="submit">Entrar na sala</button>
-                </form>
             </main>
         </div>
     )
