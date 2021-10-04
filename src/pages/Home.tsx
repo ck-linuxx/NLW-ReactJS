ec8c6dcd490890a4a32be9135bf4134a97ba7e24
ck-linuxx <you@example.com>
2021-10-04 14:24:43 -0300

    Salvar a autenticação do usuario na aplicação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useHistory } from "react-router-dom"
 
-import { auth, firebase } from "../services/firebase"
-
 import  illustrationImg from "../assets/images/illustration.svg"
 import logoImg from "../assets/images/logo.svg"
 import googleIconImg from "../assets/images/google-icon.svg"
@@ -9,21 +7,19 @@ import googleIconImg from "../assets/images/google-icon.svg"
 import { Button } from "../components/Button"
 
 import "../styles/auth.scss"
+import { useContext } from "react"
+import { AuthContext } from "../App"
 
 export function Home() {
     const  history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
+    async function handleCreateRoom(){
+        if(!user){
+            await signInWithGoogle()
+        }
 
-    function handleCreateRoom(){
-
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
-            
-            history.push("rooms/new")
-        })
-
+        history.push("/rooms/new")
     }
 
     return(
